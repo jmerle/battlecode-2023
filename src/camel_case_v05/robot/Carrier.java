ec70525993127c61d675a5c6dfde549c881a38b9
Jasper van Merle <jaspervmerle@gmail.com>
2023-01-16 02:30:37 +0100

    v05

---
+++ src/camel_case_v05/robot/Carrier.java
@@ -0,0 +1,145 @@
+package camel_case_v05.robot;
+
+import battlecode.common.Direction;
+import battlecode.common.GameActionException;
+import battlecode.common.MapLocation;
+import battlecode.common.ResourceType;
+import battlecode.common.RobotController;
+import battlecode.common.RobotInfo;
+import battlecode.common.RobotType;
+import battlecode.common.WellInfo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Carrier extends Unit {
+    private static final int CARGO_TARGET = 39;
+
+    private Map<MapLocation, WellInfo> wells = new HashMap<>();
+
+    private MapLocation hqLocation = null;
+    private boolean isCollecting = true;
+
+    public Carrier(RobotController rc) {
+        super(rc, RobotType.CARRIER);
+    }
+
+    @Override
+    public void run() throws GameActionException {
+        if (hqLocation == null) {
+            for (RobotInfo robot : rc.senseNearbyRobots(me.visionRadiusSquared, myTeam)) {
+                if (robot.type == RobotType.HEADQUARTERS) {
+                    hqLocation = robot.location;
+                    break;
+                }
+            }
+        }
+
+        lookForDangerTargets();
+
+        for (Direction direction : allDirections) {
+            MapLocation location = rc.adjacentLocation(direction);
+            if (!rc.onTheMap(location)) {
+                continue;
+            }
+
+            WellInfo well = rc.senseWell(location);
+            if (well == null) {
+                continue;
+            }
+
+            wells.put(location, well);
+        }
+
+        for (RobotInfo robot : rc.senseNearbyRobots(me.visionRadiusSquared, opponentTeam)) {
+            if (robot.type == RobotType.LAUNCHER) {
+                tryMoveToSafety();
+                break;
+            }
+        }
+
+        ResourceType resourceTarget = rc.getID() % 2 == 0 ? ResourceType.ADAMANTIUM : ResourceType.MANA;
+
+        act(resourceTarget);
+        act(resourceTarget);
+    }
+
+    private void act(ResourceType resourceTarget) throws GameActionException {
+        int cargo = getCargo();
+
+        if (isCollecting && cargo == CARGO_TARGET) {
+            isCollecting = false;
+        }
+
+        if (!isCollecting && cargo == 0) {
+            isCollecting = true;
+        }
+
+        if (isCollecting) {
+            WellInfo closestWell = null;
+            int minDistance = Integer.MAX_VALUE;
+
+            for (Map.Entry<MapLocation, WellInfo> entry : wells.entrySet()) {
+                WellInfo well = entry.getValue();
+                if (well.getResourceType() != resourceTarget) {
+                    continue;
+                }
+
+                MapLocation location = well.getMapLocation();
+                int distance = rc.getLocation().distanceSquaredTo(location);
+
+                if (closestWell == null || distance < minDistance) {
+                    closestWell = well;
+                    minDistance = distance;
+                }
+            }
+
+            if (closestWell == null) {
+                tryWander();
+            } else if (rc.getLocation().isAdjacentTo(closestWell.getMapLocation())) {
+                tryCollectResource(closestWell.getMapLocation(), Math.min(closestWell.getRate(), CARGO_TARGET - cargo));
+            } else {
+                tryMoveTo(closestWell.getMapLocation());
+            }
+        } else {
+            if (rc.getLocation().isAdjacentTo(hqLocation)) {
+                for (ResourceType resource : resourceTypes) {
+                    int amount = rc.getResourceAmount(resource);
+                    if (amount == 0) {
+                        continue;
+                    }
+
+                    if (tryTransferResource(hqLocation, resource, amount)) {
+                        break;
+                    }
+                }
+            } else {
+                tryMoveTo(hqLocation);
+            }
+        }
+    }
+
+    private int getCargo() {
+        return rc.getResourceAmount(ResourceType.ADAMANTIUM)
+                + rc.getResourceAmount(ResourceType.MANA)
+                + rc.getResourceAmount(ResourceType.ELIXIR);
+    }
+
+    private boolean tryCollectResource(MapLocation location, int amount) throws GameActionException {
+        if (rc.canCollectResource(location, amount)) {
+            rc.collectResource(location, amount);
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean tryTransferResource(MapLocation location, ResourceType type, int amount) throws GameActionException {
+        if (rc.canTransferResource(location, type, amount)) {
+            rc.transferResource(location, type, amount);
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ src/camel_case_v05/robot/Headquarters.java
@@ -0,0 +1,82 @@
+package camel_case_v05.robot;
+
+import battlecode.common.GameActionException;
+import battlecode.common.MapLocation;
+import battlecode.common.ResourceType;
+import battlecode.common.RobotController;
+import battlecode.common.RobotType;
+
+public class Headquarters extends Robot {
+    private boolean isFirstRun = true;
+
+    public Headquarters(RobotController rc) {
+        super(rc, RobotType.HEADQUARTERS);
+    }
+
+    @Override
+    public void run() throws GameActionException {
+        int turnIndex = sharedArray.getHeadquartersTurnIndex();
+        if (turnIndex == 0) {
+            sharedArray.expireDangerTargets();
+        }
+
+        if (isFirstRun) {
+            sharedArray.setMyHqLocation(turnIndex, rc.getLocation());
+
+            isFirstRun = false;
+        }
+
+        lookForDangerTargets();
+
+        if (!rc.isActionReady()) {
+            return;
+        }
+
+        RobotType type = hasResources(RobotType.LAUNCHER) ? RobotType.LAUNCHER : RobotType.CARRIER;
+        if (!hasResources(type)) {
+            return;
+        }
+
+        boolean minimizeDistanceToCenter = type == RobotType.LAUNCHER;
+        MapLocation center = new MapLocation(rc.getMapWidth() / 2, rc.getMapHeight() / 2);
+
+        MapLocation bestLocation = null;
+        int bestDistanceToCenter = minimizeDistanceToCenter ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+
+        for (MapLocation location : rc.getAllLocationsWithinRadiusSquared(rc.getLocation(), me.actionRadiusSquared)) {
+            if (!rc.canBuildRobot(type, location)) {
+                continue;
+            }
+
+            int distanceToCenter = location.distanceSquaredTo(center);
+            if ((minimizeDistanceToCenter && distanceToCenter < bestDistanceToCenter)
+                    || (!minimizeDistanceToCenter && distanceToCenter > bestDistanceToCenter)) {
+                bestLocation = location;
+                bestDistanceToCenter = distanceToCenter;
+            }
+        }
+
+        if (bestLocation != null) {
+            tryBuildRobot(type, bestLocation);
+        }
+    }
+
+    private boolean hasResources(RobotType type) {
+        for (ResourceType resource : resourceTypes) {
+            if (rc.getResourceAmount(resource) < type.getBuildCost(resource)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private boolean tryBuildRobot(RobotType type, MapLocation location) throws GameActionException {
+        if (rc.canBuildRobot(type, location)) {
+            rc.buildRobot(type, location);
+            return true;
+        }
+
+        return false;
+    }
+}
