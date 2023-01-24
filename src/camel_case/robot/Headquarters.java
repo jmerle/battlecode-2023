b96f1991d43cf7712731b7a21b0e270b6fb553bf
Jasper van Merle <jaspervmerle@gmail.com>
2023-01-24 04:31:51 +0100

    Improve build locations

--- src/camel_case/robot/Headquarters.java
+++ src/camel_case/robot/Headquarters.java
@@ -5,6 +5,7 @@ import battlecode.common.MapLocation;
 import battlecode.common.ResourceType;
 import battlecode.common.RobotController;
 import battlecode.common.RobotType;
+import battlecode.common.WellInfo;
 
 public class Headquarters extends Robot {
     private boolean isFirstRun = true;
@@ -32,28 +33,71 @@ public class Headquarters extends Robot {
             return;
         }
 
-        boolean minimizeDistanceToCenter = type == RobotType.LAUNCHER;
+        MapLocation location = null;
+
+        if (type == RobotType.CARRIER) {
+            location = getBuildLocationNearWell(ResourceType.MANA);
+            if (location == null) {
+                location = getBuildLocationNearWell(ResourceType.ADAMANTIUM);
+            }
+        }
+
+        if (location == null) {
+            location = getBuildLocationRelativeToCenter(type, type == RobotType.LAUNCHER);
+        }
+
+        if (location != null) {
+            tryBuildRobot(type, location);
+        }
+    }
+
+    private MapLocation getBuildLocationRelativeToCenter(RobotType type, boolean minimizeDistanceToCenter) throws GameActionException {
         MapLocation center = new MapLocation(rc.getMapWidth() / 2, rc.getMapHeight() / 2);
 
         MapLocation bestLocation = null;
-        int bestDistanceToCenter = minimizeDistanceToCenter ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+        int bestDistance = minimizeDistanceToCenter ? Integer.MAX_VALUE : Integer.MIN_VALUE;
 
         for (MapLocation location : rc.getAllLocationsWithinRadiusSquared(rc.getLocation(), me.actionRadiusSquared)) {
             if (!rc.canBuildRobot(type, location)) {
                 continue;
             }
 
-            int distanceToCenter = location.distanceSquaredTo(center);
-            if ((minimizeDistanceToCenter && distanceToCenter < bestDistanceToCenter)
-                    || (!minimizeDistanceToCenter && distanceToCenter > bestDistanceToCenter)) {
+            int distance = location.distanceSquaredTo(center);
+            if ((minimizeDistanceToCenter && distance < bestDistance) || (!minimizeDistanceToCenter && distance > bestDistance)) {
                 bestLocation = location;
-                bestDistanceToCenter = distanceToCenter;
+                bestDistance = distance;
             }
         }
 
-        if (bestLocation != null) {
-            tryBuildRobot(type, bestLocation);
+        return bestLocation;
+    }
+
+    private MapLocation getBuildLocationNearWell(ResourceType resource) throws GameActionException {
+        WellInfo[] wells = rc.senseNearbyWells(resource);
+        if (wells.length == 0) {
+            return null;
         }
+
+        MapLocation bestLocation = null;
+        int bestDistance = Integer.MAX_VALUE;
+
+        for (MapLocation location : rc.getAllLocationsWithinRadiusSquared(rc.getLocation(), me.actionRadiusSquared)) {
+            if (!rc.canBuildRobot(RobotType.CARRIER, location)) {
+                continue;
+            }
+
+            int distance = Integer.MAX_VALUE;
+            for (WellInfo well : wells) {
+                distance = Math.min(distance, well.getMapLocation().distanceSquaredTo(location));
+            }
+
+            if (distance < bestDistance) {
+                bestLocation = location;
+                bestDistance = distance;
+            }
+        }
+
+        return bestLocation;
     }
 
     private boolean hasResources(RobotType type) {
