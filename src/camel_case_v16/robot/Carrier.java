0aac4a26d1946de22da3f7e54d6bef08aef77885
Jasper van Merle <jaspervmerle@gmail.com>
2023-01-26 23:21:40 +0100

    v16

---
+++ src/camel_case_v16/robot/Carrier.java
@@ -0,0 +1,239 @@
+package camel_case_v16.robot;
+
+import battlecode.common.Direction;
+import battlecode.common.GameActionException;
+import battlecode.common.MapInfo;
+import battlecode.common.MapLocation;
+import battlecode.common.ResourceType;
+import battlecode.common.RobotController;
+import battlecode.common.RobotInfo;
+import battlecode.common.RobotType;
+import battlecode.common.WellInfo;
+import camel_case_v16.RobotPlayer;
+import camel_case_v16.util.MapLocationSet;
+import camel_case_v16.util.RandomUtils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Carrier extends Unit {
+    private Map<MapLocation, WellInfo> wells = new HashMap<>();
+
+    private MapLocation hqLocation = null;
+    private boolean isCollecting = true;
+
+    private MapLocationSet hasSeen = new MapLocationSet();
+    private MapLocationSet hasMarkedFrom = new MapLocationSet();
+
+    private MapLocation previousLocation = null;
+
+    public Carrier(RobotController rc) {
+        super(rc, RobotType.CARRIER);
+    }
+
+    @Override
+    public void run() throws GameActionException {
+        if (hqLocation == null) {
+            for (RobotInfo robot : rc.senseNearbyRobots(me.visionRadiusSquared, myTeam)) {
+                if (robot.type == RobotType.HEADQUARTERS) {
+                    hqLocation = robot.location;
+                    break;
+                }
+            }
+        }
+
+        for (RobotInfo robot : rc.senseNearbyRobots(me.visionRadiusSquared, opponentTeam)) {
+            if (robot.type == RobotType.LAUNCHER) {
+                if (rc.getHealth() < me.getMaxHealth()) {
+                    isCollecting = false;
+                }
+
+                tryMoveToSafety();
+                break;
+            }
+        }
+
+        act();
+        act();
+    }
+
+    private void act() throws GameActionException {
+        ResourceType resourceTarget = ResourceType.MANA;
+
+        int cargo = getCargo();
+        int cargoTarget = 30;
+
+        if (isCollecting && cargo == cargoTarget) {
+            isCollecting = false;
+        }
+
+        if (!isCollecting && cargo == 0) {
+            isCollecting = true;
+        }
+
+        if (!rc.getLocation().equals(previousLocation)) {
+            for (WellInfo well : rc.senseNearbyWells()) {
+                MapLocation location = well.getMapLocation();
+                if (wells.containsKey(location)) {
+                    if (wells.get(location) == null && !isReachable(location)) {
+                        continue;
+                    }
+
+                    wells.put(location, well);
+                } else {
+                    wells.put(location, isReachable(location) ? well : null);
+                }
+            }
+
+            previousLocation = rc.getLocation();
+        }
+
+        if (isCollecting) {
+            WellInfo closestWell = null;
+            int minDistance = Integer.MAX_VALUE;
+
+            for (Map.Entry<MapLocation, WellInfo> entry : wells.entrySet()) {
+                WellInfo well = entry.getValue();
+                if (well == null || well.getResourceType() != resourceTarget) {
+                    continue;
+                }
+
+                MapLocation location = well.getMapLocation();
+                int distance = rc.getLocation().distanceSquaredTo(location);
+
+                if (closestWell == null || distance < minDistance) {
+                    closestWell = well;
+                    minDistance = distance;
+                }
+            }
+
+            if (closestWell == null) {
+                RobotPlayer.logBytecodeWarnings = false;
+
+                if (!hasMarkedFrom.contains(rc.getLocation())) {
+                    for (MapLocation location : rc.getAllLocationsWithinRadiusSquared(rc.getLocation(), me.visionRadiusSquared)) {
+                        hasSeen.add(location);
+                    }
+
+                    hasMarkedFrom.add(rc.getLocation());
+                }
+
+                tryWander();
+            } else if (rc.getLocation().isAdjacentTo(closestWell.getMapLocation())) {
+                tryCollectResource(closestWell.getMapLocation(), Math.min(closestWell.getRate(), cargoTarget - cargo));
+            } else {
+                tryMoveTo(closestWell.getMapLocation());
+            }
+        } else {
+            if (rc.getLocation().isAdjacentTo(hqLocation)) {
+                for (ResourceType resource : resourceTypes) {
+                    int amount = rc.getResourceAmount(resource);
+                    if (amount == 0) {
+                        continue;
+                    }
+
+                    if (tryTransferResource(hqLocation, resource, amount)) {
+                        break;
+                    }
+                }
+            } else {
+                tryMoveTo(hqLocation);
+            }
+        }
+    }
+
+    @Override
+    protected MapLocation getNewWanderTarget() {
+        List<MapLocation> beacons = new ArrayList<>();
+
+        boolean foundWell = false;
+        for (Map.Entry<MapLocation, WellInfo> entry : wells.entrySet()) {
+            WellInfo well = entry.getValue();
+            if (well == null) {
+                foundWell = true;
+                continue;
+            }
+
+            if (well.getResourceType() == ResourceType.ADAMANTIUM) {
+                foundWell = true;
+                beacons.add(well.getMapLocation());
+            }
+        }
+
+        if (beacons.isEmpty() && !foundWell) {
+            beacons.add(hqLocation);
+        }
+
+        if (beacons.isEmpty()) {
+            MapLocation target = null;
+            while (target == null) {
+                MapLocation possibleTarget = super.getNewWanderTarget();
+                if (!rc.canSenseLocation(possibleTarget) && !hasSeen.contains(possibleTarget)) {
+                    target = possibleTarget;
+                }
+            }
+
+            return target;
+        }
+
+        MapLocation target = null;
+        while (target == null) {
+            MapLocation beacon = beacons.get(RandomUtils.nextInt(beacons.size()));
+            MapLocation possibleTarget = beacon.translate(RandomUtils.nextInt(-10, 11), RandomUtils.nextInt(-10, 11));
+
+            if (rc.onTheMap(possibleTarget)
+                && !rc.canSenseLocation(possibleTarget)
+                && possibleTarget.distanceSquaredTo(beacon) <= 100
+                && !hasSeen.contains(possibleTarget)) {
+                target = possibleTarget;
+            }
+        }
+
+        return target;
+    }
+
+    private boolean isReachable(MapLocation location) throws GameActionException {
+        MapLocation myLocation = rc.getLocation();
+
+        while (!myLocation.equals(location)) {
+            myLocation = myLocation.add(directionTowards(myLocation, location));
+
+            if (!rc.onTheMap(myLocation) || !rc.canSenseLocation(myLocation)) {
+                return false;
+            }
+
+            MapInfo mapInfo = rc.senseMapInfo(myLocation);
+            if (!mapInfo.isPassable() || mapInfo.getCurrentDirection() != Direction.CENTER) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private int getCargo() {
+        return rc.getResourceAmount(ResourceType.ADAMANTIUM)
+            + rc.getResourceAmount(ResourceType.MANA)
+            + rc.getResourceAmount(ResourceType.ELIXIR);
+    }
+
+    private boolean tryCollectResource(MapLocation location, int amount) throws GameActionException {
+        if (rc.canCollectResource(location, amount)) {
+            rc.collectResource(location, amount);
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean tryTransferResource(MapLocation location, ResourceType type, int amount) throws GameActionException {
+        if (rc.canTransferResource(location, type, amount)) {
+            rc.transferResource(location, type, amount);
+            return true;
+        }
+
+        return false;
+    }
+}
