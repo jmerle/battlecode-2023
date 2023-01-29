4d80cf79576f4ccaba8e00386ec84d85e3659be3
Jasper van Merle <jaspervmerle@gmail.com>
2023-01-29 05:04:56 +0100

    v24

---
+++ src/camel_case_v24/robot/Amplifier.java
@@ -0,0 +1,18 @@
+package camel_case_v24.robot;
+
+import battlecode.common.GameActionException;
+import battlecode.common.RobotController;
+import battlecode.common.RobotType;
+
+public class Amplifier extends Unit {
+    public Amplifier(RobotController rc) {
+        super(rc, RobotType.AMPLIFIER);
+    }
+
+    @Override
+    public void run() throws GameActionException {
+        super.run();
+
+        tryWander();
+    }
+}
---
+++ src/camel_case_v24/robot/Booster.java
@@ -0,0 +1,18 @@
+package camel_case_v24.robot;
+
+import battlecode.common.GameActionException;
+import battlecode.common.RobotController;
+import battlecode.common.RobotType;
+
+public class Booster extends Unit {
+    public Booster(RobotController rc) {
+        super(rc, RobotType.BOOSTER);
+    }
+
+    @Override
+    public void run() throws GameActionException {
+        super.run();
+
+        tryWander();
+    }
+}
